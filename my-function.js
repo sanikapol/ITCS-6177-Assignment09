768d9c3431241644447856b57e8e7d6bb75a8622
sanikapol <50167925+sanikapol@users.noreply.github.com>
2020-10-15 11:26:29 -0400

    Create my-function.js

---
+++ my-function.js
@@ -0,0 +1,10 @@
+//Lambda Function
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Sanika!'),
+    };
+    return response;
+};
+
