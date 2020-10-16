ac0b296baf799d04b7b6e00ba085e52abbed8787
sanikapol <50167925+sanikapol@users.noreply.github.com>
2020-10-15 23:19:14 -0400

    Update my-function.js

--- my-function.js
+++ my-function.js
@@ -8,3 +8,28 @@ exports.handler = async (event) => {
     return response;
 };
 
+// Endpoint
+
+app.get(('/say'),cache(120), async (req,res) => {
+    let conn;
+    try{
+            axios.get('https://gzoyyxfsi1.execute-api.us-east-1.amazonaws.com/Test1/events')
+            .then ((response) => {
+                console.log(response);
+                res.status(200);
+                res.setHeader('Content-Type', 'application/json');
+                res.send("Sanika says " + req.query.keyword);
+            })
+            .catch((error) => {
+                console.log(error + " from axios");
+            });
+
+    }catch (err){
+           console.log(err);
+            throw err;
+    } finally {
+            if (conn) return conn.end();
+    }
+})
+
+
